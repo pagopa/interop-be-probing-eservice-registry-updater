461a30144b5ab6127bc84463b6e09f50df5f5f06
CSCMWS\acolella <alesandro.colella@dxc.com>
2023-04-03 10:51:20 +0200

    fixed strong typing upon fields

--- src/test/java/it/pagopa/interop/probing/eservice/registry/updater/unit/util/RestClientTest.java
+++ src/test/java/it/pagopa/interop/probing/eservice/registry/updater/unit/util/RestClientTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -38,10 +39,10 @@ class RestClientTest {
 	@BeforeEach
 	void setup() throws IOException {
 		String[] basePath = { "basePath1", "basePath2" };
-		eServiceDTO = EserviceDTO.builder().eserviceId("0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7")
-				.versionId("226574b8-82a1-4844-9484-55fffc9c15ef").name("Service Name").producerName("Producer Name")
-				.state(EserviceState.fromValue("ACTIVE").getValue())
-				.technology(EserviceTechnology.fromValue("REST").getValue()).basePath(basePath).versionNumber("1")
+		eServiceDTO = EserviceDTO.builder().eserviceId(UUID.fromString("0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7"))
+				.versionId(UUID.fromString("226574b8-82a1-4844-9484-55fffc9c15ef")).name("Service Name").producerName("Producer Name")
+				.state(EserviceState.ACTIVE)
+				.technology(EserviceTechnology.REST).basePath(basePath).versionNumber("1")
 				.build();
 	}
 
