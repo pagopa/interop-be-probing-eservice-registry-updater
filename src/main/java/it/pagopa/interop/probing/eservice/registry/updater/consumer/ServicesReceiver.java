d8449087a11d0d057ae784fbd51ce91612be9ed3
angelaantonia.cea <angelaantonia.cea@dxc.com>
2023-05-25 15:13:13 +0200

    Added Guice library to implement the Dependency Injection pattern

--- src/main/java/it/pagopa/interop/probing/eservice/registry/updater/consumer/ServicesReceiver.java
+++ src/main/java/it/pagopa/interop/probing/eservice/registry/updater/consumer/ServicesReceiver.java
@@ -3,60 +3,51 @@ package it.pagopa.interop.probing.eservice.registry.updater.consumer;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
-import java.util.Objects;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 import feign.Feign;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 import feign.okhttp.OkHttpClient;
 import it.pagopa.interop.probing.eservice.registry.updater.client.EserviceClient;
-import it.pagopa.interop.probing.eservice.registry.updater.config.PropertiesLoader;
-import it.pagopa.interop.probing.eservice.registry.updater.config.aws.sqs.SqsConfig;
 import it.pagopa.interop.probing.eservice.registry.updater.dto.impl.EserviceDTO;
 import it.pagopa.interop.probing.eservice.registry.updater.util.logging.Logger;
-import it.pagopa.interop.probing.eservice.registry.updater.util.logging.impl.LoggerImpl;
 
-
+@Singleton
 public class ServicesReceiver {
 
-  private final Logger logger = new LoggerImpl();
-  private static ServicesReceiver instance;
+  @Inject
+  private Logger logger;
 
-  private static final String SQS = "amazon.sqs.endpoint.services-queue";
-  private static final String ESERVICE_OPERATION_URL = "api.operations.baseUrl";
-  private static final String ESERVICE_BASE_PATH = "api.operations.eservice.basePath";
+  @Inject
+  private AmazonSQSAsync sqs;
 
-  public static ServicesReceiver getInstance() throws IOException {
-    if (Objects.isNull(instance)) {
-      instance = new ServicesReceiver();
-    }
-    return instance;
+  @Inject
+  private ObjectMapper mapper;
 
-  }
-
+  @Inject
+  @Named("amazon.sqs.endpoint.services-queue")
   private String sqsUrlServices;
-  private String eserviceOperationUrl;
-  private String eserviceBasePath;
 
-  public ServicesReceiver() throws IOException {
-    this.sqsUrlServices = PropertiesLoader.getInstance().getKey(SQS);
-    this.eserviceOperationUrl = PropertiesLoader.getInstance().getKey(ESERVICE_OPERATION_URL);
-    this.eserviceBasePath = PropertiesLoader.getInstance().getKey(ESERVICE_BASE_PATH);
-  }
+  @Inject
+  @Named("api.operations.baseUrl")
+  private String eserviceOperationUrl;
+
+  @Inject
+  @Named("api.operations.eservice.basePath")
+  private String eserviceBasePath;
 
   public void receiveStringMessage() throws IOException {
 
-
-    ObjectMapper mapper = new ObjectMapper();
-    SqsConfig sqs = SqsConfig.getInstance();
-
     ReceiveMessageRequest receiveMessageRequest =
         new ReceiveMessageRequest(sqsUrlServices).withMaxNumberOfMessages(10);
-    List<Message> sqsMessages =
-        sqs.getAmazonSQSAsync().receiveMessage(receiveMessageRequest).getMessages();
+    List<Message> sqsMessages = sqs.receiveMessage(receiveMessageRequest).getMessages();
 
     while (!sqsMessages.isEmpty()) {
       for (Message message : sqsMessages) {
@@ -77,13 +68,13 @@ public class ServicesReceiver {
         }
         logger.logMessageEserviceSaved(service.getEserviceId(), service.getVersionId());
 
-        sqs.getAmazonSQSAsync().deleteMessage(new DeleteMessageRequest()
-            .withQueueUrl(sqsUrlServices).withReceiptHandle(message.getReceiptHandle()));
+        sqs.deleteMessage(new DeleteMessageRequest().withQueueUrl(sqsUrlServices)
+            .withReceiptHandle(message.getReceiptHandle()));
 
         logger.logMessageQueueMessageDeleted(service.getEserviceId(), service.getVersionId(),
             URI.create(sqsUrlServices));
       }
-      sqsMessages = sqs.getAmazonSQSAsync().receiveMessage(receiveMessageRequest).getMessages();
+      sqsMessages = sqs.receiveMessage(receiveMessageRequest).getMessages();
     }
 
   }
--- src/test/java/it/pagopa/interop/probing/eservice/registry/updater/unit/dto/EserviceDTOTest.java
+++ src/test/java/it/pagopa/interop/probing/eservice/registry/updater/unit/dto/EserviceDTOTest.java
@@ -2,16 +2,13 @@
 package it.pagopa.interop.probing.eservice.registry.updater.unit.dto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
 import java.util.UUID;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import it.pagopa.interop.probing.eservice.registry.updater.dto.impl.EserviceDTO;
 import it.pagopa.interop.probing.eservice.registry.updater.util.EserviceState;
 import it.pagopa.interop.probing.eservice.registry.updater.util.EserviceTechnology;
@@ -19,34 +16,41 @@ import it.pagopa.interop.probing.eservice.registry.updater.util.EserviceTechnolo
 @ExtendWith(MockitoExtension.class)
 class EserviceDTOTest {
 
-	private EserviceDTO eServiceDTO;
+  private EserviceDTO eServiceDTO;
 
-	@BeforeEach
-	void setup() throws IOException {
-		String[] basePath = { "basePath1", "basePath2" };
-		eServiceDTO = EserviceDTO.builder().eserviceId(UUID.fromString("0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7"))
-				.versionId(UUID.fromString("226574b8-82a1-4844-9484-55fffc9c15ef")).name("Service Name").producerName("Producer Name")
-				.state(EserviceState.ACTIVE)
-				.technology(EserviceTechnology.REST).basePath(basePath).versionNumber(1)
-				.build();
-	}
+  @BeforeEach
+  void setup() throws IOException {
+    String[] basePath = {"basePath1", "basePath2"};
+    eServiceDTO = new EserviceDTO();
+    eServiceDTO.setEserviceId(UUID.fromString("0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7"));
+    eServiceDTO.setVersionId(UUID.fromString("226574b8-82a1-4844-9484-55fffc9c15ef"));
+    eServiceDTO.setName("Service Name");
+    eServiceDTO.setProducerName("Producer Name");
+    eServiceDTO.setState(EserviceState.fromValue("ACTIVE"));
+    eServiceDTO.setTechnology(EserviceTechnology.fromValue("REST"));
+    eServiceDTO.setBasePath(basePath);
+    eServiceDTO.setVersionNumber(1);
+  }
 
-	@Test
-	@DisplayName("Test the utility toString of lombok.")
-	void testToString_whenGivenValidEServiceDto_thenValidEquals() throws IOException {
-		String serviceString = "EserviceDTO(name=Service Name, eserviceId=0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7, versionId=226574b8-82a1-4844-9484-55fffc9c15ef, technology=REST, state=ACTIVE, basePath=[basePath1, basePath2], producerName=Producer Name, versionNumber=1)";
-		assertEquals(eServiceDTO.toString(), serviceString);
-	}
+  @Test
+  @DisplayName("Test the utility toString of lombok.")
+  void testToString_whenGivenValidEServiceDto_thenValidEquals() throws IOException {
+    String serviceString =
+        "EserviceDTO(name=Service Name, eserviceId=0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7, versionId=226574b8-82a1-4844-9484-55fffc9c15ef, technology=REST, state=ACTIVE, basePath=[basePath1, basePath2], producerName=Producer Name, versionNumber=1)";
+    assertEquals(eServiceDTO.toString(), serviceString);
+  }
 
-	@Test
-	@DisplayName("Test the utility Equals and HashCode of lombok.")
-	void testEqualsHashCode_whenGivenValidEServiceDto_thenValidEquals() throws IOException {
-		String[] basePath = { "basePath1", "basePath2" };
+  @Test
+  @DisplayName("Test the utility Equals and HashCode of lombok.")
+  void testEqualsHashCode_whenGivenValidEServiceDto_thenValidEquals() throws IOException {
+    String[] basePath = {"basePath1", "basePath2"};
 
-		EserviceDTO copy = EserviceDTO.builder().eserviceId(UUID.fromString("0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7"))
-				.versionId(UUID.fromString("226574b8-82a1-4844-9484-55fffc9c15ef")).name("Service Name").producerName("Producer Name")
-				.state(EserviceState.ACTIVE).technology(EserviceTechnology.REST).basePath(basePath).versionNumber(1).build();
-		assertEquals(true, eServiceDTO.equals(copy));
-		assertEquals(true, eServiceDTO.hashCode() == copy.hashCode());
-	}
+    EserviceDTO copy =
+        EserviceDTO.builder().eserviceId(UUID.fromString("0b37ac73-cbd8-47f1-a14c-19bcc8f8f8e7"))
+            .versionId(UUID.fromString("226574b8-82a1-4844-9484-55fffc9c15ef")).name("Service Name")
+            .producerName("Producer Name").state(EserviceState.ACTIVE)
+            .technology(EserviceTechnology.REST).basePath(basePath).versionNumber(1).build();
+    assertEquals(true, eServiceDTO.equals(copy));
+    assertEquals(true, eServiceDTO.hashCode() == copy.hashCode());
+  }
 }
