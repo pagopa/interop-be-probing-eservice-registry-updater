e423c117230950c62ef1c1a4247af56267121db6
melissa-dellorco <102969723+melissa-dellorco@users.noreply.github.com>
2023-05-26 10:11:23 +0200

    feature/IESP-208 (#19)
    Co-authored-by: Melissa <melissadellorco1993@gmail.com>

--- src/main/java/it/pagopa/interop/probing/eservice/registry/updater/dto/impl/EserviceDTO.java
+++ src/main/java/it/pagopa/interop/probing/eservice/registry/updater/dto/impl/EserviceDTO.java
@@ -1,13 +1,14 @@
 
 package it.pagopa.interop.probing.eservice.registry.updater.dto.impl;
 
-import it.pagopa.interop.probing.eservice.registry.updater.dto.Dto;
+import java.util.UUID;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import it.pagopa.interop.probing.eservice.registry.updater.annotations.ValidateStringArraySize;
+import it.pagopa.interop.probing.eservice.registry.updater.dto.Dto;
 import it.pagopa.interop.probing.eservice.registry.updater.util.EserviceState;
 import it.pagopa.interop.probing.eservice.registry.updater.util.EserviceTechnology;
 import lombok.AllArgsConstructor;
@@ -18,8 +19,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.UUID;
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -29,32 +28,36 @@ import java.util.UUID;
 @Builder
 public class EserviceDTO implements Dto {
 
-	@NotBlank(message = "must not be blank")
-	@Size(max = 255, message = "must not be longer than 255 chars")
-	private String name;
+  @NotBlank(message = "must not be blank")
+  @Size(max = 255, message = "must not be longer than 255 chars")
+  private String name;
 
-	@NotNull(message = "must not be null")
-	private UUID eserviceId;
+  @NotNull(message = "must not be null")
+  private UUID eserviceId;
 
-	@NotNull(message = "must not be null")
-	private UUID versionId;
+  @NotNull(message = "must not be null")
+  private UUID versionId;
 
-	@NotNull(message = "must not be null")
-	private EserviceTechnology technology;
+  @NotNull(message = "must not be null")
+  private EserviceTechnology technology;
 
-	@NotNull(message = "must not be null")
-	private EserviceState state;
+  @NotNull(message = "must not be null")
+  private EserviceState state;
 
-	@NotEmpty(message = "list cannot be empty")
-	@ValidateStringArraySize(maxSize = 2048)
-	private String[] basePath;
+  @NotEmpty(message = "list cannot be empty")
+  @ValidateStringArraySize(maxSize = 2048)
+  private String[] basePath;
 
-	@NotBlank(message = "must not be blank")
-	@Size(max = 255, message = "must not be longer than 255 chars")
-	private String producerName;
+  @NotBlank(message = "must not be blank")
+  @Size(max = 255, message = "must not be longer than 255 chars")
+  private String producerName;
 
-	@NotNull(message = "must not be null")
-	@Min(value=1, message="must be at least 1")
-	private Integer versionNumber;
+  @NotNull(message = "must not be null")
+  @Min(value = 1, message = "must be at least 1")
+  private Integer versionNumber;
+
+  @NotEmpty(message = "list cannot be empty")
+  @ValidateStringArraySize(maxSize = 2048)
+  private String[] audience;
 
 }
